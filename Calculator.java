fed3e0d1d4ec8b6c5477aacca0049ff980cb4675
dev1 <dev1@gmail.com>
2025-04-17 15:28:22 +0100

    feat : implement substraction feature
     This PR implements the basic addition operation in the Calculator class. Includes manual test using CLI menu.

--- Calculator.java
+++ Calculator.java
@@ -4,4 +4,14 @@ public class Calculator {
     public int addition( int a , int b ){
         return a + b ; 
     }
+
+    public int substraction( int number1 , int number2 ){
+        return number1 - number2 ; 
+    }
+
+    public static void main( String[] args ){
+
+        Calculator claculator = new Calculator() ; 
+        System.out.println( claculator.substraction( 2 , 9 )) ; 
+    }
 }
